3fbd717c6f98ddb62b514ab4e59f8db7d4b3f14e
bhreina <bhreina.k@gmail.com>
2023-05-12 16:48:24 +0100

    home-office-visa-check

---
+++ src/main/java/gov/uk/check/visa/pages/StartPage.java
@@ -0,0 +1,26 @@
+package gov.uk.check.visa.pages;
+
+import com.aventstack.extentreports.Status;
+import gov.uk.check.visa.customlisteners.CustomListeners;
+import gov.uk.check.visa.utilities.Utility;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
+
+public class StartPage extends Utility {
+
+    public StartPage() {
+        PageFactory.initElements(driver, this);
+    }
+    @CacheLookup
+    @FindBy(xpath = "//body/div[@id='wrapper']/div[1]/main[1]/div[2]/div[1]/article[1]/section[1]/a[1]")
+    WebElement start;
+
+    public void clickStartNow(){
+        Reporter.log("Click on start now " + start.toString());
+        clickOnElement(start);
+        CustomListeners.test.log(Status.PASS, "Click on start now ");
+    }
+}
